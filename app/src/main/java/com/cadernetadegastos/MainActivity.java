e111e21663a1f122971030a0958785f02ab61d39
celso <celso4rruda@gmail.com>
2020-09-12 15:44:29 -0300

    vamos tentar fazer isso errado

--- app/src/main/java/com/cadernetadegastos/MainActivity.java
+++ app/src/main/java/com/cadernetadegastos/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //codigo recebido, celso
+    //vamos tentar fazer isso errado
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
