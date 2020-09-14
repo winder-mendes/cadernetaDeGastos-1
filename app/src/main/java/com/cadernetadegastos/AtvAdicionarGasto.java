92083b830951f5c9e352646a61a6b6f65c051bfc
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2020-09-14 18:29:53 -0300

    criando tela de adicinar gasto

---
+++ app/src/main/java/com/cadernetadegastos/AtvAdicionarGasto.java
@@ -0,0 +1,14 @@
+package com.cadernetadegastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtvAdicionarGasto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atv_adicionar_gasto);
+    }
+}
