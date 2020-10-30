cf013d62c0dd460b4abb09617d199acc6f30f006
celso <celso4rruda@gmail.com>
2020-10-30 17:40:20 -0300

    iniciando alterar renda

--- app/src/main/java/com/cadernetadegastos/AlterarRenda.java
+++ app/src/main/java/com/cadernetadegastos/AlterarRenda.java
@@ -3,12 +3,34 @@ package com.cadernetadegastos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AlterarRenda extends AppCompatActivity {
+public class AlterarRenda extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTexNovaRenda;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alterar_renda);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+        editTexNovaRenda = findViewById(R.id.editTexNovaRenda);
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == button){
+            
+        }
+
     }
 }
