8d10c69ce6a001f8d24971e2adbb674fedc4f0d6
celso <celso4rruda@gmail.com>
2020-11-09 13:52:29 -0300

    calculo concluido, ainda precisa ser revisado

--- app/src/main/java/com/cadernetadegastos/Calculo.java
+++ app/src/main/java/com/cadernetadegastos/Calculo.java
@@ -1,21 +1,59 @@
 package com.cadernetadegastos;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Calculo extends AppCompatActivity {
+import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.Month;
 
-    //Gastos(vindo do bd) - Renda(vindo do bd)
+import java.util.Date;
+import java.util.List;
+
+public class Calculo {
+
+    private List<Lancamento> lista;
+    private float renda;
+
+    public Calculo(List<Lancamento> lista, float renda) {
+        this.lista = lista;
+        this.renda = renda;
+    }
+
+    public float retornaRenda() {
+        return renda;
+    }
 
 
-    public float CalcularDiferenca(){
-        UsuarioDao ud = new UsuarioDao(this);
-        LancamentoDao ld = new LancamentoDao(this);
-        Usuario user = new Usuario();
-        float b = user.getRenda();
-        Lancamento l = new Lancamento();
-        float a = l.getValor();
-        float resultado = b - a;
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public float retornSaldo() {
+        float gasto = gastoMensal();
+        float resultado = renda - gasto;
         return resultado;
+    }
+
+    public float retornaGastoTotal() {
+        float somaTotal = 0;
+        for (Lancamento lancamento : lista) {
+            somaTotal = somaTotal + lancamento.getValor();
+        }
+        return somaTotal;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public float gastoMensal() {
+        float gastoMes = 0;
+        LocalDate currentdate = LocalDate.now();
+        Month currentMonth = currentdate.getMonth();
+        for (Lancamento lancamento : lista) {
+            Month dtLancamento = Month.of(lancamento.getData().getMonth());
+            if(dtLancamento == currentMonth){
+                gastoMes = gastoMes + lancamento.getValor();
+            }
+        }
+        return gastoMes;
     }
 
 }
